ef3ac7b9141ad7264cc4250456cbdbe464ec526a
Tom Wash <Tommyjohn2006@gmail.com>
2020-05-28 17:33:09 -0400

    pre clear blobs process

--- SikkaApi/ApiInterface.js
+++ SikkaApi/ApiInterface.js
@@ -84,7 +84,10 @@ async function getBaseResourceByRequestKeyAndDumpToBlob(request_key, resourceUri
     }
 
     if (data[0]) {
-        const items = data[0].items.map((item) => {
+        const items = data[0].items.map((item, index) => {
+            // if (item && index === 0) {
+            //     createMergeQuery(item, )
+            // }
             return JSON.stringify(item)
         });
         const listOfBlockIds = [];
@@ -115,8 +118,6 @@ async function getBaseResourceByRequestKeyAndDumpToBlob(request_key, resourceUri
                     blockId = Buffer.from(uuidV4()).toString('base64');
                     const stageResult = await writeStreamInStagedBlocks(blockId, itemsInLoop.join(','), blobLocation);
                     listOfBlockIds.push(blockId);
-                    // console.log(`stage of blockId: ${blockId} successful\n\n\n current blockid list values are: ${listOfBlockIds}\n\n\n stage block response \n\n`);
-                    // console.log(stageResult);
                 } catch (err) {
                     console.log(err);
                 }
@@ -125,7 +126,6 @@ async function getBaseResourceByRequestKeyAndDumpToBlob(request_key, resourceUri
 
         const commitBlocksResult = await commitStagedBlocks(listOfBlockIds, blobLocation);
         console.log(`commit of blockid list successful:\n\n ${listOfBlockIds}\n\n\n commit block response \n\n`);
-        // console.log(commitBlocksResult);
 
         return [];
     }
@@ -175,10 +175,195 @@ async function commitStagedBlocks(listOfBlockIds, blobLocation) {
     return await blockBlobClient.commitBlockList(listOfBlockIds);
 }
 
+async function clearBlobs() {
+    // Create the BlobServiceClient object which will be used to create a container client
+    const blobServiceClient = await BlobServiceClient.fromConnectionString(connectionString);
+
+    // // Get a reference to a container
+    const containerClient = await blobServiceClient.getContainerClient(containerName);
+    const blobs = containerClient.listBlobsFlat();
+    for await (const blob of blobs) {
+        if (blob.toString().includes('streams')) { continue; }
+        await containerClient.deleteBlob(blob);
+    }
+
+
+}
+
+
+async function createMergeQuery(resourceResponse, tableName, office_id, practice_name) {
+
+    const fileLocation = `sql`;
+    const fileName = `${tableName}.sql`
+    const pathToFile = `${fileLocation}/${fileName}`
+
+    // Create the BlobServiceClient object which will be used to create a container client
+    const blobServiceClient = await BlobServiceClient.fromConnectionString(connectionString);
+
+    // // Get a reference to a container
+    const containerClient = await blobServiceClient.getContainerClient(containerName);
+
+    // Get blob block client, used modifying blobs in azure storage
+    const blockBlobClient = await containerClient.getBlockBlobClient(pathToFile)
+
+    const blobExists = await blockBlobClient.exists();
+
+    if (blobExists) { return };
+
+    if (resourceResponse.href) {
+
+        // ======================================== //
+        // ======================================== //
+        // =========== Setup query params ========= //
+        // ======================================== //
+        // ======================================== //
+        const currentDate = new Date();
+        const currentIsoDateTime = currentDate.toISOString();
+
+        let insertColumns = '';
+        let columnsStringCreateTable = '';
+        let updateQuery = '';
+        let withOpenJson = '';
+        let mergeQueryInsertValuesFromModified = '';
+        Object.keys(resourceResponse).forEach(key => {
+            insertColumns += `[col_${key}],`;
+            columnsStringCreateTable += `col_${key} varchar(8000),`;
+            updateQuery += `original.[col_${key}] = modified.[col_${key}],`;
+            mergeQueryInsertValuesFromModified += `modified.[col_${key}],`;
+            withOpenJson += `col_${key} varchar(8000) '$.${key}',`;
+        })
+
+        // add metadata fields
+        columnsStringCreateTable += 'created_at datetime, updated_at datetime, office_id varchar(2000), practice_name varchar(2000),'
+        insertColumns += 'created_at, updated_at, office_id, practice_name,'
+        updateQuery +=
+            `
+                            original.updated_at = '${currentIsoDateTime}',
+                            original.office_id = '${office_id}',
+                            practice_name = '${practice_name.replace(/'/g, "''")}',`
+        mergeQueryInsertValuesFromModified +=
+            `
+                            '${currentIsoDateTime}',
+                            '${currentIsoDateTime}',
+                            '${office_id}',
+                            '${practice_name.replace(/'/g, "''")}',`
+
+        // slice off trailing comma
+        columnsStringCreateTable = columnsStringCreateTable.slice(0, -1);
+        insertColumns = insertColumns.slice(0, -1);
+        updateQuery = updateQuery.slice(0, -1);
+        mergeQueryInsertValuesFromModified = mergeQueryInsertValuesFromModified.slice(0, -1)
+        withOpenJson = withOpenJson.slice(0, -1);
+
+        const query =
+            `
+                            -- Declare JSON Variable
+                            declare @json nvarchar(max) = 
+                            (
+                                SELECT
+                                   '[' + CAST(BulkColumn AS NVARCHAR(MAX)) + ']' AS JsonData 
+                                FROM 
+                                OPENROWSET(BULK 'oakpoint-data/streams/${office_id}/${tableName}.json', DATA_SOURCE = 'OakpointDataV1', SINGLE_CLOB) AS AzureBlob 
+                            ); 
+    
+                            -- Declare Temp Table
+                            Declare @TableView TABLE 
+                            ( 
+                                ID bigint NOT NULL PRIMARY KEY IDENTITY(1, 1),
+                                ${columnsStringCreateTable}
+                            ); 
+    
+                            INSERT INTO @TableView
+                                (${insertColumns})
+                                SELECT *,
+                                '${currentIsoDateTime}',
+                                '${currentIsoDateTime}',
+                                '${office_id}',
+                                '${practice_name.replace(/'/g, "''")}' FROM OPENJSON(@json)  
+                                WITH  
+                                (
+                                        ${withOpenJson}
+                                );
+    
+                            WITH CTE AS(
+                                SELECT col_href,
+                                    RN = ROW_NUMBER()OVER(PARTITION BY col_href ORDER BY col_href)
+                                FROM @TableView
+                            )
+                            DELETE FROM CTE WHERE RN > 1
+    
+                            MERGE ${tableName} original
+                            USING @TableView modified
+                            ON original.col_href = modified.col_href
+                            WHEN MATCHED 
+                                THEN UPDATE SET 
+                                ${updateQuery}
+                            WHEN NOT MATCHED BY TARGET THEN
+                            INSERT  
+                            (
+                                ${insertColumns}
+                            )
+                            VALUES
+                            (
+                                ${mergeQueryInsertValuesFromModified}
+                            );
+                        `
+        // ======================================== //
+        // ================ Execute =============== //
+        // =========== Create Table Query ========= //
+        // ======================================== //
+        // ======================================== //
+        try {
+            if (tableName) {
+                const tableCreateQuery = `
+                            IF(object_id('${tableName}', 'U') is null) BEGIN
+                            print 'Must create the table!';
+                            CREATE TABLE ${tableName} (
+                                ID bigint NOT NULL PRIMARY KEY IDENTITY(1, 1),
+                                ${columnsStringCreateTable}
+                            );
+    
+                            CREATE UNIQUE INDEX col_href
+                            ON ${tableName} (col_href)
+    
+                            CREATE INDEX non_unique_office_metadata
+                            ON ${tableName} (office_id, practice_name, created_at, updated_at)
+                            END
+                            `
+                const pool = new sql.ConnectionPool(azureSqlConfig);
+                await pool.connect();
+                try {
+                    await pool.query(tableCreateQuery);
+                } catch (err) {
+                    console.error(err);
+                }
+                await pool.close();
+
+            } else {
+                throw new Error(tableName)
+            }
+        }
+        catch (err) {
+            context.log(err);
+        }
+
+
+        // ======================================== //
+        // ======================================== //
+        // =========== Upload Sql File ============ //
+        // ======================================== //
+        // ======================================== //
+        // Create file for office to store the JSON response and prep for upload
+
+        await blockBlobClient.upload(query.toString(), query.toString().length);
+    }
+}
+
 
 
 module.exports = {
     authorizedPractices,
+    clearBlobs,
     dataCheck,
     healthCheck,
     requestKey,
